4005022ab624275ba619032a74557dde01dd601c
parthabathia <parthabathia@gmail.com>
2023-12-14 18:47:05 +0530

    Updated README.md and TreasureIsland.py

---
+++ Day3/README.md
@@ -0,0 +1,7 @@
+# Day 2 of 100 days of Python programming bootcamp
+
+This repository contains a Python Code for a mini game Treasure Island.
+
+The game requires users to choose from various paths to survive and win the game. The game also provides an option to restart the game if you die. It is a small and interesting game.
+
+If you are interested in playin, feel free to use the code and experience it yourself. Also, feel free to add more forks and paths to make the game more interesting.
---
+++ Day3/TreasureIsland.py
@@ -0,0 +1,85 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("\nWelcome to Treasure Island.")
+start = input("Do you wish to start? (Y/N) ")
+play = 1
+if start == 'Y':
+    while play:
+        print("Let's Start")
+        direction = input("Do you wish to go Left or Right? (L/R) ")
+        if direction == 'R':
+            print("You fell into a valley. You Died!")
+            print("Game Over.")
+            play_again = input("Do you wish to try again? (Y/N) ")
+            if play_again == 'Y':
+                continue
+            else:
+                print("Sorry to see you go.")
+                break
+        else:
+            print("You chose the correct path.")
+            print("You arrived at a lake.")
+            pass_the_lake = input("Do you wish to wait or swim? (W/S) ")
+            if pass_the_lake == 'S':
+                print("You were eaten by pirhanas. You Died!")
+                print("Game Over.")
+                play_again = input("Do you wish to try again? (Y/N) ")
+                if play_again == 'Y':
+                    continue
+                else:
+                    print("Sorry to see you go.")
+                    break
+            else:
+                print("You waited for the boat.")
+                print("You crossed the lake.")
+                print("There are three doors in front of you. Red, Yellow and Blue.")
+                gate = input("Which door will you select? (R/Y/B) ")
+                if gate == 'R':
+                    print("You entered a room full of fire. You died.")
+                    print("Game Over.")
+                    play_again = input("Do you wish to try again? (Y/N) ")
+                    if play_again == 'Y':
+                        continue
+                    else:
+                        print("Sorry to see you go.")
+                        break
+                elif gate == 'B':
+                    print("You entered a room full of poison. You died.")
+                    print("Game Over.")
+                    play_again = input("Do you wish to try again? (Y/N) ")
+                    if play_again == 'Y':
+                        continue
+                    else:
+                        print("Sorry to see you go.")
+                        break
+                else:
+                    print("Congratulation! You have won.")
+                    print("Game over.")
+                    play_again = input("Do you wish to play again? (Y/N) ")
+                    if play_again == 'Y':
+                        continue
+                    else:
+                        print("Sorry to see you go.")
+                        break
+else:
+    print("Sorry to see you go.")
